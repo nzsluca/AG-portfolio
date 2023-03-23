ff07cddafdeeb1488d8432ff0b54f8dd36a12a79
nzsluca <n.zs.luca@gmail.com>
2023-03-23 10:34:46 +0100

    fisrt push

--- src/gallerys/scroll/scroll-galery.tsx
+++ src/gallerys/scroll/scroll-galery.tsx
@@ -8,7 +8,7 @@ import * as aranykapu2 from "../assets/creative/aranykapu/arany2.jpg"
 import * as aranykapu3 from "../assets/creative/kerdezz_anyamrol/arany1.jpg"
 import * as aranykapu4 from "../assets/creative/kerdezz_anyamrol/arany.jpg"
 import { everySeries } from '../../utilities'
-
+import {getAlbumss} from '../../services/gallery-service'
 
 
 
@@ -32,6 +32,7 @@ function Scroll() {
         threshold: 0.5,
     }
     React.useEffect(() => {
+        getAlbumss()
         const observer = new IntersectionObserver(titleChange, option)
         if(titleRef.current) observer.unobserve(titleRef.current)
         return() => {
@@ -48,15 +49,15 @@ function Scroll() {
          <>
            <span className='landing-title'  ref={titleRef} >{title}</span>
 
-                <div id='image-box' >
-                    {everySeries[0].image.map((e,i) => {
-                        return (< div key={1+rand}> 
+                <div id='image-conatiner' >
+                    {/* {everySeries[0].image.map((e,i) => {
+                        return (< div className='image-box' key={1+rand}> 
                         <img className='image' key={i + rand} src={e} alt=""/>
                         <div className='desc-box'  key={i+rand}>
                         <span className='desc-box-name'>{everySeries[0].year}</span>
                         <span className='desc-box-date'>Lorem ipsum dolor sit.</span>
                     </div></div>  )
-                    })}
+                    })} */}
                
                 </div>
            </>
